bcb65d0061fc3c60760f013769873d3273269771
Sameer99Pro <samirshaikhh0306@gmail.com>
2026-05-04 13:13:10 +0530

    Fix CORS for production

--- src/main/java/com/jobtracker/job_tracker_backend/controller/AuthController.java
+++ src/main/java/com/jobtracker/job_tracker_backend/controller/AuthController.java
@@ -12,7 +12,11 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = {
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://job-tracker-frontend-zeta-wine.vercel.app"
+})
 public class AuthController {
 
     @Autowired
--- src/main/java/com/jobtracker/job_tracker_backend/controller/JobApplicationController.java
+++ src/main/java/com/jobtracker/job_tracker_backend/controller/JobApplicationController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/applications")
-@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174" , "https://job-tracker-frontend-zeta-wine.vercel.app"})
 public class JobApplicationController {
 
     @Autowired
